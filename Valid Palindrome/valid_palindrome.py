bf9761918bb3fe24e2fbf830bad3d5c2e93da0e1
aabboud1 <abboudahmed1@gmail.com>
2021-04-01 21:29:53 +0100

    working code problem solved

--- Valid Palindrome/valid_palindrome.py
+++ Valid Palindrome/valid_palindrome.py
@@ -1,2 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        new_string = []
+        
+        for x in s:
+            if (ord(x) > 64 and ord(x) < 91) or (ord(x) > 96 and ord(x) < 123) or (ord(x) > 47 and ord(x) < 58):
+                new_string.append(x.lower())
+        
+        n = len(new_string)
+        
+        half = n // 2
+        
+        for x in range(half):
+            if new_string[x] != new_string[n - 1 - x]:
+                return False
+        return True
