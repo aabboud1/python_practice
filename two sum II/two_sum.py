e3e094ed80be41d34d98ef97dd5acacf1b3b168b
aabboud1 <abboudahmed1@gmail.com>
2021-04-10 23:44:58 +0100

    working solutin with while loop

--- two sum II/two_sum.py
+++ two sum II/two_sum.py
@@ -3,3 +3,11 @@ class Solution:
         n = len(numbers)
         i = 0
         j = n - 1
+
+        while i < j:
+            if numbers[i] + numbers[j] > target:
+                j -= 1
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+            else:
+                return i + 1, j + 1 
