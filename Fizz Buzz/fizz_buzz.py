1775344504e4820bf4fb7c5dd2b16de721201b65
aabboud1 <abboudahmed1@gmail.com>
2021-04-18 07:38:58 -0400

    using loop to go through

--- Fizz Buzz/fizz_buzz.py
+++ Fizz Buzz/fizz_buzz.py
@@ -1,3 +1,15 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
          li = []
+
+         for x in range(1, n + 1):
+            if x % 3 == 0 and x % 5 == 0:
+                li.append('FizzBuzz')
+            elif x % 3 == 0:
+                li.append('Fizz')
+            elif x % 5 == 0:
+                li.append('Buzz')
+            else:
+                li.append(str(x))
+        
+        return li
