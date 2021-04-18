0ec8b32b1237fe1db64e855d3c5c5f32d917d212
aabboud1 <abboudahmed1@gmail.com>
2021-04-18 07:40:20 -0400

    working code with notes

--- Fizz Buzz/fizz_buzz.py
+++ Fizz Buzz/fizz_buzz.py
@@ -1,7 +1,10 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
+         
+        #  create empty list to store 
          li = []
 
+        # loop to go through the list and check the number and write the correct thing
          for x in range(1, n + 1):
             if x % 3 == 0 and x % 5 == 0:
                 li.append('FizzBuzz')
